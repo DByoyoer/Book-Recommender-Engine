cccd58a9fc082b17d853938ec64fa0fdad11cbba
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-03-22 16:27:27 -0700

    Add pages to book model

--- app/models/book.py
+++ app/models/book.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from . import Base
 
+# TODO: Convert this to association object to include an 'order' field to indicate correct order to list the authors
 book_author_association = Table("book_authors", Base.metadata,
                                 Column("book_id", ForeignKey("book.id"), primary_key=True),
                                 Column("author_id", ForeignKey("author.id"), primary_key=True)
@@ -22,6 +23,7 @@ class Book(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     isbn: Mapped[Optional[str]] = mapped_column(String(12))
     lang_code: Mapped[str | None] = mapped_column(String(10))
+    pages: Mapped[int | None]
     title: Mapped[str]
     description: Mapped[str | None]
     cover_url: Mapped[str | None]
