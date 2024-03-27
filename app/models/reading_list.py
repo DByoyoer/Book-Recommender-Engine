78d33f4b556bb6c7fa1cce864ac1358b31fcb8d0
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-03-27 15:32:43 -0700

    Add default value for ranking in reading_list model

--- app/models/reading_list.py
+++ app/models/reading_list.py
@@ -10,6 +10,6 @@ class ReadingList(Base):
     book_id: Mapped[int] = mapped_column(ForeignKey("book.id"), primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), primary_key=True)
 
-    ranking: Mapped[int]
+    ranking: Mapped[int] = mapped_column(default=1)
     date_added: Mapped[datetime] = mapped_column(server_default=func.now())
     user: Mapped["User"] = relationship(back_populates="reading_list")
