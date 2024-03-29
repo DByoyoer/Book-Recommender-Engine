08b98a20e63cbe49adaf6347b431127254ac05d9
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-03-28 19:35:53 -0700

    Reorder column definitions

--- app/models/rating.py
+++ app/models/rating.py
@@ -8,10 +8,10 @@ from . import Base
 class Rating(Base):
     __tablename__ = "rating"
 
-    book_id: Mapped[int] = mapped_column(ForeignKey("book.id"), primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), primary_key=True)
-    rating_text: Mapped[str | None]
+    book_id: Mapped[int] = mapped_column(ForeignKey("book.id"), primary_key=True)
     rating: Mapped[int]
+    rating_text: Mapped[str | None]
     date_created: Mapped[datetime] = mapped_column(server_default=func.now())
     date_updated: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
 
--- app/models/reading_list.py
+++ app/models/reading_list.py
@@ -7,9 +7,9 @@ from . import Base
 
 class ReadingList(Base):
     __tablename__ = "reading_list"
-    book_id: Mapped[int] = mapped_column(ForeignKey("book.id"), primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), primary_key=True)
 
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), primary_key=True)
+    book_id: Mapped[int] = mapped_column(ForeignKey("book.id"), primary_key=True)
     ranking: Mapped[int] = mapped_column(server_default=text("0"))
     date_added: Mapped[datetime] = mapped_column(server_default=func.now())
     user: Mapped["User"] = relationship(back_populates="reading_list")
