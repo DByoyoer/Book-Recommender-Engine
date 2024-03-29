4172a510f486be6ac256c1a734fec69a5a902a77
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-03-28 19:34:40 -0700

    Fix ranking not having default on database's side

--- app/models/reading_list.py
+++ app/models/reading_list.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import ForeignKey, func
+from sqlalchemy import ForeignKey, func, text, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from . import Base
@@ -10,6 +10,6 @@ class ReadingList(Base):
     book_id: Mapped[int] = mapped_column(ForeignKey("book.id"), primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), primary_key=True)
 
-    ranking: Mapped[int] = mapped_column(default=1)
+    ranking: Mapped[int] = mapped_column(server_default=text("0"))
     date_added: Mapped[datetime] = mapped_column(server_default=func.now())
     user: Mapped["User"] = relationship(back_populates="reading_list")
