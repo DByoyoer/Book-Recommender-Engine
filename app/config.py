acbae358fd518e164579b2522c015cd76ba769f5
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-04-01 12:55:58 -0700

    Add defaults to Settings and it also now reads from a .env file

--- app/config.py
+++ app/config.py
@@ -1,8 +1,10 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file="./.env")
     database_url: str
-    test: bool
-    api_name: "Book Recommender"
+    test: bool = False
+    echo_sql: bool = False
+    api_name:str = "Book Recommender"
 
 settings = Settings()
