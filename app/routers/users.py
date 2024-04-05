2cb701eb6b952a8d8d46c42582ca269df2f34e7d
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-04-05 01:32:02 -0700

    Add error checking for non-existent user_id

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -22,12 +22,16 @@ async def get_user_info(user_id: int, db_session: AsyncSession = Depends(get_db_
 @router.get("/{user_id}/ratings", response_model=list[RatingSchema])
 async def get_user_ratings(user_id: int, db_session: AsyncSession = Depends(get_db_session)):
     user = await db_session.get(User, user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
     return user.ratings
 
 
 @router.get("/{user_id}/reading_list", response_model=list[ReadingListEntrySchema])
 async def get_user_reading_list(user_id: int, db_session: AsyncSession = Depends(get_db_session)):
     user = await db_session.get(User, user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
     return user.reading_list
 
 
