c38d54a5ffe58f086c6dd60411462c70b8eb18e3
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-04-06 01:03:25 -0700

    Configure project to run using docker containers

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from routers import authors, books, users
 
@@ -11,3 +12,6 @@ app.include_router(users.router)
 @app.get("/")
 async def root():
     return {"message": "This is a WIP book recommendation API."}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8000)
